5723498470f206e2049c5c975810dcaf28cbdfdd
Anzah Khan <anzahkhan@protonmail.com>
2024-02-07 10:54:03 +1000

    Created component and added test

---
+++ tests/MCQ.test.ts
@@ -0,0 +1,9 @@
+import { mount } from "@vue/test-utils";
+import MCQ from "../src/components/MCQ.vue";
+
+describe("MCQ.vue", () => {
+  test("Renders component", () => {
+    const wrapper = mount(MCQ);
+    expect(wrapper.text()).toContain("MCQ Test");
+  });
+});
