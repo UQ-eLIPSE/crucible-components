d8f511e3ca3d7546a5efcd59b177fc8fc1064f2b
Anzah Khan <anzah.khan@uq.edu.au>
2024-02-08 12:40:12 +1000

    Added path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       "@components": fileURLToPath(
         new URL("./src/components", import.meta.url),
       ),
+      "@data": fileURLToPath(new URL("./data", import.meta.url)),
     },
   },
   test: {
