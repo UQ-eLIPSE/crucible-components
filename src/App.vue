98c4e23a3835edf1abdf374878a65d5b6c436fa3
anzau <36717729+anzau@users.noreply.github.com>
2024-03-06 14:33:54 +1000

    Story/vethub 33 (#15)
    * Added input for selecting number of questions to answer
    
    * Added styling
    
    * WIP - still fixing
    
    * WIP
    
    * Fixed component testing
    
    * Fixed comments
    
    ---------
    
    Co-authored-by: Anzah Khan <anzah.khan@uq.edu.au>
    Co-authored-by: Anzah Khan <anzahkhan@protonmail.com>

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,28 @@
 import { ref } from "vue";
 import MCQQuiz from "@components/MCQ/MCQQuiz.vue";
 import { questions } from "@data/question-data.json";
+import StartPage from "./components/MCQ/StartPage.vue";
 const showQuiz = ref(false);
+const quizQuestions = ref(questions);
 
-const startQuiz = () => {
+const handleStartQuiz = (questionAmount: number) => {
+  quizQuestions.value = questions.slice(0, questionAmount);
   showQuiz.value = true;
 };
 </script>
 
 <template>
-  <MCQQuiz v-if="showQuiz" :questions="questions" />
-  <div v-else>
-    <h1>Welcome to VetsCloud Smart Quiz</h1>
-    <button class="start-button" @click="startQuiz">Start</button>
-  </div>
+  <MCQQuiz v-if="showQuiz" :questions="quizQuestions" />
+  <StartPage v-else :questions="questions" @start-quiz="handleStartQuiz" />
 </template>
 
 <style scoped>
+#question-amount {
+  margin-left: 5px;
+}
 .start-button {
   color: #ffffff;
   background-color: #2a52be;
+  margin-top: 5%;
 }
 </style>
