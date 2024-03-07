54a4e66f58657e6b787cb3cb40c313b60b9a9a99
anzau <36717729+anzau@users.noreply.github.com>
2024-03-07 10:46:21 +1000

    Story/VETHUB-32 (#16)
    * Created Start button and moved styling along with created tests.
    
    * Able to show tags, WIP for integration within the quiz
    
    * Added input for selecting number of questions to answer
    
    * Added styling
    
    * WIP - still fixing
    
    * WIP
    
    * Fixed component testing
    
    * Reduced tag scope, added simple styling, added testing
    
    * Simplifying
    
    * Made changes according to PR comments
    
    ---------
    
    Co-authored-by: Anzah Khan <anzah.khan@uq.edu.au>
    Co-authored-by: Anzah Khan <anzahkhan@protonmail.com>

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,13 @@ import StartPage from "./components/MCQ/StartPage.vue";
 const showQuiz = ref(false);
 const quizQuestions = ref(questions);
 
-const handleStartQuiz = (questionAmount: number) => {
-  quizQuestions.value = questions.slice(0, questionAmount);
+const handleStartQuiz = (questionAmount: number, tag?: string) => {
+  if (tag)
+    quizQuestions.value = quizQuestions.value
+      .filter((question) => question.tags && question.tags.includes(tag))
+      .slice(0, questionAmount);
+  else quizQuestions.value = quizQuestions.value.slice(0, questionAmount);
+
   showQuiz.value = true;
 };
 </script>
@@ -21,9 +26,17 @@ const handleStartQuiz = (questionAmount: number) => {
 #question-amount {
   margin-left: 5px;
 }
+
 .start-button {
   color: #ffffff;
   background-color: #2a52be;
   margin-top: 5%;
+}
+
+.tag-list {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+  gap: 10px;
 }
 </style>
--- src/components/MCQ/StartPage.vue
+++ src/components/MCQ/StartPage.vue
@@ -2,6 +2,16 @@
   <div>
     <h1>Welcome to VetsCloud Smart Quiz</h1>
     <div>
+      <label for="question-amount">Enter a tag:</label>
+      <input
+        id="question-tag"
+        v-model="tag"
+        type="text"
+        name="tags"
+        placeholder="Enter your tag"
+      />
+    </div>
+    <div>
       <label for="question-amount"> Select the amount of questions:</label>
       <input
         id="question-amount"
@@ -21,21 +31,22 @@ import { MCQQuiz } from "@/types/MCQ";
 import { ref } from "vue";
 const { questions } = defineProps<MCQQuiz>();
 const questionAmount = ref<number>(0);
-
+const tag = ref<string>("");
 const emit = defineEmits(["start-quiz"]);
 
 const startQuiz = () => {
-  emit("start-quiz", questionAmount.value);
+  emit("start-quiz", questionAmount.value, tag.value);
 };
 </script>
 
 <style scoped>
-#question-amount {
+#question-amount,
+#question-tag {
   margin-left: 5px;
+  margin-bottom: 5%;
 }
 .start-button {
   color: #ffffff;
   background-color: #2a52be;
-  margin-top: 5%;
 }
 </style>
