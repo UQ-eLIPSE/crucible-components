69f576c843f1942b7bc81f449a6a2070f7ba7276
Mohamad <moh.dabboussi@gmail.com>
2024-04-05 10:27:43 +1000

    Added test case for Quiz Mode

--- tests/components/StartPage.test.ts
+++ tests/components/StartPage.test.ts
@@ -14,12 +14,23 @@ describe("StartPage.vue", () => {
     expect(wrapper.find("button").text()).toBe("Start");
   });
 
-  it("Sets input value in the range of the amount of questions. ", async () => {
+  it("Sets input value in the range of the amount of questions.", async () => {
     const wrapper = mount(StartPage, {
       props: { questions },
     });
     const input = wrapper.find("#question-amount");
     await input.setValue("2");
     expect(wrapper.vm.questionAmount).toBe(2);
+  });
+
+  it("Allows selection between Tutor mode and Timed mode", async () => {
+    const wrapper = mount(StartPage, {
+      props: { questions },
+    });
+    const select = wrapper.find("#mode-select");
+    await select.setValue("Timed");
+    expect(wrapper.vm.selectedMode).toBe("Timed");
+    await select.setValue("Tutor");
+    expect(wrapper.vm.selectedMode).toBe("Tutor");
   });
 });
