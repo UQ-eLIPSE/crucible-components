d8938115503ac15d4f1546e6684811cac72a9331
Chia-Chuan Tsou (Jason) <121275444+freshc0w@users.noreply.github.com>
2024-04-11 14:03:55 +1000

    feature/VETHUB-63 (#40)
    * Add validation of links and populate question-data.js based on json file'
    
    * Add comments and refactor retryValidation
    
    * Revert package.json
    
    * Revert port v2
    
    * Fix bug where if there's no badlink, an error occurs
    
    * Refactor validateQuizLink function
    
    * Change const naming to camelCase
    
    * add ts-node
    
    * run script
    
    ---------
    
    Co-authored-by: jtsou <{ID}+{username}@users.noreply.github.com>
    Co-authored-by: Joanna Zhong <joanna.zhong@uq.edu.au>

---
+++ data/validateQuestionLink.js
@@ -0,0 +1,90 @@
+import { questions } from "./question-data.js";
+import fs from "fs/promises";
+import path from "path";
+
+const statusSuccess = 200;
+
+// log file: create directory if it doesn't exist
+const logsDirectory = "./logs"; // Directory to store logs
+const logsPath = path.join(logsDirectory, "broken-links-logs.csv");
+const ensureLogsDirectoryExists = async () => {
+  try {
+    await fs.access(logsDirectory);
+  } catch (error) {
+    // Logs directory doesn't exist, create it
+    await fs.mkdir(logsDirectory);
+  }
+};
+
+// The input param is the link to be validated on.
+export const validateURLResponse = async (input) => {
+  try {
+    const response = await fetch(input);
+
+    const status = response.status;
+
+    return status === statusSuccess;
+  } catch (error) {
+    console.error(
+      `An error occured while accessing "${input}". \nError: ${error}`,
+    );
+    return false;
+  }
+};
+
+const validateQuizLink = async (linkList) => {
+  console.info("validating links...", linkList.length);
+  const badLinks = await Promise.all(
+    linkList.map(async ({ link }, idx) => {
+      const result = await validateURLResponse(link);
+      if (!result) {
+        console.error("Found invalid link:", link, "at index", idx);
+        return { link };
+      }
+      return null;
+    }),
+  );
+  return badLinks.filter((link) => link);
+};
+
+async function logError(errorLog, message) {
+  await fs.appendFile(errorLog, message + "\n");
+}
+
+/**
+ * Sometimes multiple retries in validation of links is required since there
+ * are too many links. This retries all the bad ones depending on the trialTimes
+ * provided.
+ *
+ * If length are too long, can use the trialTimes parameter
+ * i.e. retryValidation(badLinks, badLinks.length / 10)
+ */
+const retryValidation = async (badLinks, trialTimes = 3) => {
+  if (!badLinks.length) return console.info("No need to retry validation");
+
+  let n = 0;
+  while (n <= trialTimes && badLinks.length) {
+    console.info("Retrying...", ++n);
+    console.info("Testing number of badlinks:", badLinks.length);
+    badLinks = await validateQuizLink(badLinks);
+  }
+
+  return badLinks;
+};
+
+async function main() {
+  await ensureLogsDirectoryExists();
+
+  let badLinks = await validateQuizLink(questions);
+
+  badLinks = await retryValidation(badLinks);
+
+  const resourcesLinksLogs = path.resolve(logsPath);
+  await fs.writeFile(resourcesLinksLogs, "Link\n"); // header
+  if (!badLinks) return console.info("No bad links. Done.");
+  badLinks.forEach(async ({ link }) => {
+    await logError(resourcesLinksLogs, `${link}`);
+  });
+}
+
+main();
