f5202d9a272ebea47516bcb7aea1a87b5242ea0b
jtsou <{ID}+{username}@users.noreply.github.com>
2024-04-17 12:32:19 +1000

    Transfer quiz logic to timed quiz to encapsulate logic

--- src/components/MCQ/MCQTimedQuiz.vue
+++ src/components/MCQ/MCQTimedQuiz.vue
@@ -1,20 +1,54 @@
 <template>
   <h3 v-if="timeLeft">Time left: {{ formatSecondsToMinutes(timeLeft) }}</h3>
-  <MCQQuiz />
+  <MCQQuestion
+    v-if="currentQuestion"
+    :statement="currentQuestion.statement"
+    :options-list="currentQuestion.optionsList"
+    :_id="currentQuestion._id"
+    @next-question="nextQuestion"
+    @skip-question="skipQuestion"
+  />
+  <MCQStatus v-if="!currentQuestion" />
+  <button v-if="!currentQuestion" class="btn-relocate" @click="refreshPage">
+    End
+  </button>
 </template>
 
 <script setup lang="ts">
 import { useQuizStore } from "@/store/QuizStore";
-import MCQQuiz from "./MCQQuiz.vue";
-import { onBeforeMount, onBeforeUnmount, ref } from "vue";
+import MCQQuestion from "@components/MCQ/MCQQuestion.vue";
+import MCQStatus from "./MCQStatus.vue";
+import { onBeforeMount, onBeforeUnmount, onMounted, ref } from "vue";
+import { MCQuestion } from "@/types/MCQ";
 
 const oneSecond = 1000;
 
-const quizStore = useQuizStore();
+const questionsQueue = useQuizStore();
+const currentQuestion = ref<MCQuestion | undefined>();
 
 let timeoutId: number | null = null;
 let intervalId: number | null = null;
-const timeLeft = ref(quizStore.timeLimit);
+const timeLeft = ref(questionsQueue.timeLimit);
+
+onMounted(() => {
+  nextQuestion();
+});
+
+const nextQuestion = () =>
+  (currentQuestion.value = questionsQueue.dequeueQuestion());
+
+const skipQuestion = () => {
+  if (!currentQuestion.value) {
+    console.error(
+      "Attempting to skip a question when no remaining questions are available.",
+    );
+    return;
+  }
+  questionsQueue.enqueueQuestion(currentQuestion.value as MCQuestion);
+  nextQuestion();
+};
+
+const refreshPage = () => window.location.reload();
 
 const resetTimer = () => {
   timeoutId && clearTimeout(timeoutId);
@@ -22,13 +56,17 @@ const resetTimer = () => {
 };
 
 const startTimer = () => {
-  timeLeft.value = quizStore.timeLimit;
+  timeLeft.value = questionsQueue.timeLimit;
 
   intervalId = window.setInterval(() => {
+    if (!timeLeft.value) {
+      resetTimer();
+      return;
+    }
     timeLeft.value--;
   }, oneSecond);
 
-  timeoutId = window.setTimeout(() => {}, quizStore.timeLimit * oneSecond);
+  timeoutId = window.setTimeout(() => {}, questionsQueue.timeLimit * oneSecond);
 };
 
 const performTimerActions = () => {
@@ -52,4 +90,18 @@ const formatSecondsToMinutes = (time: number) => {
 };
 </script>
 
-<style></style>
+<style>
+.btn-relocate {
+  float: right;
+  background-color: green;
+  color: white;
+  padding: 6px 12px;
+  text-align: center;
+  border: 1px solid #7e7e7e;
+  border-radius: 5px;
+  width: fit-content;
+  margin: auto;
+  margin-bottom: 5px;
+  cursor: pointer;
+}
+</style>
