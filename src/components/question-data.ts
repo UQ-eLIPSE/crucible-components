dedc2eab6f6b47fd9fd59db6c5c1da199c410d60
Mohamad <moh.dabboussi@gmail.com>
2024-05-14 11:07:34 +1000

    Added tests

--- src/components/question-data.ts
+++ src/components/question-data.ts
@@ -129,4 +129,284 @@ export const pluginQuestions = [
     label: "Placeholder label 1",
     link: "https://vetshub.uqcloud.net/resource/5a0ba18d34cc363763e05e99/61a9ae14e04e3d5bffb26ef7/62142eee64c71f1df2110cf5/62142f2764c71f1df2110cf7/6214407964c71f1df2110d03",
   },
+  {
+    tags: ["course: VETS2012", "subject:Physiology", "animal:dog"],
+    statement: "<p>When is it impossible to generate an action potential?</p>",
+    optionsList: [
+      {
+        optionValue: "<p>Absolute refractory period</p>",
+        optionCorrect: true,
+      },
+      {
+        optionValue: "<p>Relative refractory period</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>Sodium conductance</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>EPSPs after IPSPs</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>Hyperpolarised state</p>",
+        optionCorrect: false,
+      },
+    ],
+    _id: {
+      $oid: "6615c7fb49fbda0108a9ac0f",
+    },
+    label: "Placeholder label 1",
+    link: "https://vetshub.uqcloud.net/resource/5a0ba18d34cc363763e05e99/61a9ae14e04e3d5bffb26ef7/62142eee64c71f1df2110cf5/62142f2764c71f1df2110cf7/6214407964c71f1df2110d03",
+  },
+  {
+    tags: ["course: VETS2013", "subject:Physiology"],
+    statement:
+      "<p>Which of the following types of glial cells myelinate neurons in the peripheral nervous system?</p>",
+    optionsList: [
+      {
+        optionValue: "<p>Ependymal cells</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>Schwann cells</p>",
+        optionCorrect: true,
+      },
+      {
+        optionValue: "<p>Muller cells</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>Oligodendrocytes</p>",
+        optionCorrect: false,
+      },
+    ],
+    _id: {
+      $oid: "6615c7fb49fbda0108a9ac11",
+    },
+    label: "Placeholder label 1",
+    link: "https://vetshub.uqcloud.net/resource/5a0ba18d34cc363763e05e99/61a9ae14e04e3d5bffb26ef7/62142eee64c71f1df2110cf5/62142f2764c71f1df2110cf7/6214413464c71f1df2110d07",
+  },
+  {
+    tags: ["course: VETS2012", "subject:Atonomy", "animal:Horse"],
+    statement:
+      "<p>Which of the following is an attribute of ependymal cells?</p>",
+    optionsList: [
+      {
+        optionValue: "<p>Repair processes</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>Cerebrospinal fluid synthesis</p>",
+        optionCorrect: true,
+      },
+      {
+        optionValue: "<p>Delivering nutrients</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>Recycling of neurotransmitters</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>Maintenance of terminal environment</p>",
+        optionCorrect: false,
+      },
+    ],
+    _id: {
+      $oid: "6615c7fb49fbda0108a9ac10",
+    },
+    label: "Placeholder label 1",
+    link: "https://vetshub.uqcloud.net/resource/5a0ba18d34cc363763e05e99/61a9ae14e04e3d5bffb26ef7/62142eee64c71f1df2110cf5/62142f2764c71f1df2110cf7/6214413464c71f1df2110d07",
+  },
+  {
+    tags: ["course: VETS2013", "subject:Physiology", "animal:cat"],
+    statement:
+      "<p>Depending on the pre-synaptic neurotransmitter released and the post-synaptic receptor activated, the post-synaptic membrane can either be depolarised or hyperpolarised. Which of the following statements is FALSE?</p>",
+    optionsList: [
+      {
+        optionValue:
+          "<p>Neurotransmitter binding to metabotropic receptors causes a conformational change in pore proteins</p>",
+        optionCorrect: true,
+      },
+      {
+        optionValue:
+          "The action of metabotropic receptors is slower than ionotropic receptors",
+        optionCorrect: false,
+      },
+      {
+        optionValue:
+          "Neurotransmitter binding to metabotropic receptors causes a conformational change, activating a signal transduction pathway",
+        optionCorrect: false,
+      },
+      {
+        optionValue:
+          "An example of an ionotropic receptor is the nicotinic acetylcholine receptor",
+        optionCorrect: false,
+      },
+    ],
+    _id: {
+      $oid: "6539c0bdeb2b18699ed86faa",
+    },
+    label: "Placeholder label 1",
+    link: "https://vetshub.uqcloud.net/resource/5a0ba18d34cc363763e05e99/61a9ae14e04e3d5bffb26ef7/62142eee64c71f1df2110cf5/62183be064c71f1df2110d0f/62183d5a64c71f1df2110d16",
+  },
+  {
+    tags: ["course: VETS2012", "subject:Physiology", "animal:Horse"],
+    statement:
+      "Nicotine (mimics acetylcholine) can bind to nicotinic cholinergic receptors. You would expect the response on the post-synaptic membrane to be:",
+    optionsList: [
+      {
+        optionValue: "Excitation due to opening of chloride channels",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "Hyperpolarisation due to blocking of sodium channels",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "Hyperpolarisation due to opening of chloride channels",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "Excitation due to opening of sodium channels",
+        optionCorrect: true,
+      },
+    ],
+    _id: {
+      $oid: "6539c0bdeb2b18699ed86fac",
+    },
+    label: "Placeholder label 1",
+    link: "https://vetshub.uqcloud.net/resource/5a0ba18d34cc363763e05e99/61a9ae14e04e3d5bffb26ef7/62142eee64c71f1df2110cf5/62183be064c71f1df2110d0f/62183d5a64c71f1df2110d16",
+  },
+  {
+    tags: ["course: VETS2012", "subject:Physiology", "animal:Horse"],
+    statement:
+      "&gamma;-aminobutyric acid (GABA) is a rapidly acting neurotransmitter. You would expect the response on the post-synaptic membrane to be:",
+    optionsList: [
+      {
+        optionValue: "Excitation due to opening of chloride channels",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "Hyperpolarisation due to blocking of sodium channels",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "Hyperpolarisation due to opening of chloride channels",
+        optionCorrect: true,
+      },
+      {
+        optionValue: "Excitation due to opening of sodium channels",
+        optionCorrect: false,
+      },
+    ],
+    _id: {
+      $oid: "6539c0bdeb2b18699ed86fab",
+    },
+    label: "Placeholder label 1",
+    link: "https://vetshub.uqcloud.net/resource/5a0ba18d34cc363763e05e99/61a9ae14e04e3d5bffb26ef7/62142eee64c71f1df2110cf5/62183be064c71f1df2110d0f/62183d5a64c71f1df2110d16",
+  },
+  {
+    tags: ["course: VETS2012", "subject:Physiology", "animal:Horse"],
+    statement:
+      "<p>At the neuromuscular junction, Ca<sup>2+</sup>&nbsp;ions are necessary for:</p>",
+    optionsList: [
+      {
+        optionValue:
+          "<p>Binding the transmitter with the post-synaptic receptor</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue:
+          "<p>Facilitating diffusion of the transmitter to the post-synaptic membrane</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue:
+          "<p>Splitting the neurotransmitter in the synaptic cleft, deactivating the transmitter</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue:
+          "<p>Fusing the pre-synaptic vesicle with the pre-synaptic membrane, thus releasing the transmitter</p>",
+        optionCorrect: true,
+      },
+      {
+        optionValue:
+          "<p>Metabolising the transmitter within the pre-synaptic vesicle</p>",
+        optionCorrect: false,
+      },
+    ],
+    _id: {
+      $oid: "6539c0bdeb2b18699ed86fae",
+    },
+    label: "Placeholder label 1",
+    link: "https://vetshub.uqcloud.net/resource/5a0ba18d34cc363763e05e99/61a9ae14e04e3d5bffb26ef7/62142eee64c71f1df2110cf5/62183be064c71f1df2110d0f/62183d7064c71f1df2110d17",
+  },
+  {
+    tags: ["course: VETS2012", "subject:Physiology", "animal:Horse"],
+    statement:
+      "<p>Which of the following statements is TRUE with regard to the termination of the synaptic action at the neuromuscular junction?</p>",
+    optionsList: [
+      {
+        optionValue:
+          "<p>The re-uptake of intact acetylcholine molecules into the motor neuron terminal is responsible</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue:
+          "<p>Diffusion of acetylcholine away from the synapse is solely responsible</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue:
+          "<p>Acetylcholinesterase rapidly breaks down acetylcholine into choline and acetate</p>",
+        optionCorrect: true,
+      },
+      {
+        optionValue:
+          "<p>Dissociation of acetylcholine from the muscarinic receptor, after binding for several seconds, is solely responsible</p>",
+        optionCorrect: false,
+      },
+    ],
+    _id: {
+      $oid: "6539c0bdeb2b18699ed86fad",
+    },
+    label: "Placeholder label 1",
+    link: "https://vetshub.uqcloud.net/resource/5a0ba18d34cc363763e05e99/61a9ae14e04e3d5bffb26ef7/62142eee64c71f1df2110cf5/62183be064c71f1df2110d0f/62183d7064c71f1df2110d17",
+  },
+  {
+    tags: ["course: VETS2012", "subject:Physiology", "animal:Horse"],
+    statement:
+      "<p>Which part of a neuron receives information from surrounding cells?</p>",
+    optionsList: [
+      {
+        optionValue: "<p>Axon</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>Presynaptic terminal</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>Cell body</p>",
+        optionCorrect: false,
+      },
+      {
+        optionValue: "<p>Dendrite</p>",
+        optionCorrect: true,
+      },
+      {
+        optionValue: "<p>Myelin</p>",
+        optionCorrect: false,
+      },
+    ],
+    _id: {
+      $oid: "6615c7fb49fbda0108a9ac06",
+    },
+    label: "Placeholder label 1",
+    link: "https://vetshub.uqcloud.net/resource/5a0ba18d34cc363763e05e99/61a9ae14e04e3d5bffb26ef7/62142eee64c71f1df2110cf5/62142f2764c71f1df2110cf7/6214301b64c71f1df2110cfd",
+  },
 ];
--- tests/components/StartPage.test.ts
+++ tests/components/StartPage.test.ts
@@ -4,11 +4,12 @@ import { questionsData as questions } from "../testSeeds";
 import StartPage from "@components/StartPage.vue";
 import { createPinia, setActivePinia } from "pinia";
 import { useQuizStore } from "../../src/store/QuizStore";
+import { getConvertedStaticData } from "../../src/components/DataAccessLayer";
 
 beforeEach(() => {
   setActivePinia(createPinia());
   const questionsQueue = useQuizStore();
-  questionsQueue.allQs = questions;
+  questionsQueue.allQs = getConvertedStaticData();
 });
 
 describe("StartPage.vue", () => {
@@ -24,7 +25,7 @@ describe("StartPage.vue", () => {
 
   it("Sets input value in the range of the amount of questions.", async () => {
     const wrapper = mount(StartPage, {
-      props: { questions },
+      props: {},
     });
     const input = wrapper.find("#question-amount");
     await input.setValue("2");
@@ -33,12 +34,30 @@ describe("StartPage.vue", () => {
 
   it("Allows selection between Tutor mode and Timed mode", async () => {
     const wrapper = mount(StartPage, {
-      props: { questions },
+      props: {},
     });
     const select = wrapper.find("#mode-select");
     await select.setValue("Timed");
     expect(wrapper.vm.selectedMode).toBe("Timed");
     await select.setValue("Tutor");
     expect(wrapper.vm.selectedMode).toBe("Tutor");
+  });
+
+  it("Sets the default value of questionAmount to 10 if maximum questions is greater than 10", () => {
+    const wrapper = mount(StartPage, {
+      props: {},
+    });
+
+    expect(wrapper.vm.questionAmount).toBe(10);
+  });
+
+  it("Updates questionAmount if maximum questions changes", async () => {
+    const wrapper = mount(StartPage, {
+      props: {},
+    });
+    const quizStore = useQuizStore();
+    quizStore.allQs = getConvertedStaticData().slice(0, 5);
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.questionAmount).toBe(5);
   });
 });
